7eb7e6e0b4669ebda8d31057e5e284fc4799b9e9
sorinupwork <sorin.upwork@gmail.com>
2020-04-19 22:11:57 +0300

    add login and logout

---
+++ app.js
@@ -0,0 +1,73 @@
+const createError    = require('http-errors');
+const express        = require('express');
+const path           = require('path');
+const cookieParser   = require('cookie-parser');
+const logger         = require('morgan');
+const bodyparser     = require('body-parser');
+const passport       = require('passport');
+const User = require('./models/user');
+const session = require('express-session');
+const mongoose = require('mongoose');
+
+//require routes
+const index   = require('./routes/index');
+const posts   = require('./routes/posts');
+const reviews = require('./routes/reviews');
+
+const app = express();
+
+//connect to the database
+mongoose.connect('mongodb://localhost:27017/surf-shop', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('we\'re connected!')
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Configure passport and sessions
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//Mount routes
+app.use('/', index);
+app.use('/posts', posts);
+app.use('/posts/:id/reviews', reviews);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+const User = require('../models/user')
+
+module.exports = {
+  async postRegister(req, res, next) {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    image: req.body.image
+  });
+
+    await User.register(newUser, req.body.password);
+    res.redirect('/');
+  }
+}
