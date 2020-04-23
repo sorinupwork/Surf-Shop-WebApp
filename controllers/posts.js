58e664ff52b0ec756df6c698f50e1b30f23e21a0
sorinupwork <sorin.upwork@gmail.com>
2020-04-24 02:52:51 +0300

    bug fixes inputs

--- controllers/posts.js
+++ controllers/posts.js
@@ -95,7 +95,7 @@ module.exports = {
     post.description = req.body.post.description
     post.price = req.body.post.price
     post.properties.description = `<strong><a href="/posts/${post._id}">${post.title}</a></strong><p>${post.location}</p><p>${post.description.substring(0, 20)}...</p>`;
-    post.save();
+    await post.save();
     res.redirect(`/posts/${post.id}`);
   },
   //Posts Destroy
