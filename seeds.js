6c53a5275e3cd036936aa61e19f98125c3e18817
sorinupwork <sorin.upwork@gmail.com>
2020-04-24 02:20:24 +0300

    add authorization to posts

--- seeds.js
+++ seeds.js
@@ -16,10 +16,7 @@ async function seedPosts() {
 				type: 'Point',
 				coordinates: [cities[random1000].longitude, cities[random1000].latitude],
 			},
-			author: {
-		    '_id' : '5e9f3f4524257819b059e8b2',
-		    'username' : 'ian'
-		  }
+			author: "5ea1e147b1f39d3294f7b457"
 		}
 		let post = new Post(postData);
 		post.properties.description = `<strong><a href="/posts/${post._id}">${title}</a></strong><p>${post.location}</p><p>${description.substring(0, 20)}...</p>`;
